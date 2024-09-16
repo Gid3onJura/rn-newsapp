50ee3f34feedeb6360039288913a6be0cdcf8d1f
Gid3onJura <homebasekorlash@googlemail.com>
2024-09-16 23:04:55 +0200

    loading indicator, news list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native"
+import { ActivityIndicator, ScrollView, StyleSheet, Text, View } from "react-native"
 import React, { useEffect, useState } from "react"
 import { useSafeAreaInsets } from "react-native-safe-area-context"
 import Header from "@/components/Header"
@@ -8,16 +8,19 @@ import { NewsDataType } from "@/types"
 import BreakingNews from "@/components/BreakingNews"
 import Categories from "@/components/Categories"
 import NewsList from "@/components/NewsList"
+import Loading from "@/components/Loading"
 
 type Props = {}
 
 const Page = (props: Props) => {
   const { top: safeTop } = useSafeAreaInsets()
   const [breakingNews, setBreakingNews] = useState<NewsDataType[]>([])
+  const [news, setNews] = useState<NewsDataType[]>([])
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     getBreakingNews()
+    getNews()
   }, [])
 
   const getBreakingNews = async () => {
@@ -26,7 +29,7 @@ const Page = (props: Props) => {
         process.env.EXPO_PUBLIC_NEWS_DATA_API_BASE_URL +
         "/latest?apikey=" +
         process.env.EXPO_PUBLIC_NEWS_DATA_API_KEY +
-        "&country=de&language=de&image=1&removeduplicate=1&size=5"
+        "&language=de&image=1&removeduplicate=1&size=5"
 
       const response = await axios.get(newsDataUrl)
 
@@ -39,18 +42,37 @@ const Page = (props: Props) => {
     }
   }
 
+  const getNews = async () => {
+    try {
+      const newsDataUrl =
+        process.env.EXPO_PUBLIC_NEWS_DATA_API_BASE_URL +
+        "/latest?apikey=" +
+        process.env.EXPO_PUBLIC_NEWS_DATA_API_KEY +
+        "&language=de&image=1&removeduplicate=1&size=10"
+
+      const response = await axios.get(newsDataUrl)
+
+      if (response && response.data) {
+        setNews(response.data.results)
+        // setIsLoading(false)
+      }
+    } catch (error: any) {
+      console.log("Error in getNews:", error.message)
+    }
+  }
+
   const onCategoryChanged = (category: string) => {
     console.log(category)
   }
 
   return (
-    <View style={(styles.container, { paddingTop: safeTop })}>
+    <ScrollView style={(styles.container, { paddingTop: safeTop })}>
       <Header />
       <SearchBar />
-      {isLoading ? <ActivityIndicator size={"large"} /> : <BreakingNews newsList={breakingNews} />}
+      {isLoading ? <Loading size={"large"} /> : <BreakingNews newsList={breakingNews} />}
       <Categories onCategoryChanged={onCategoryChanged} />
-      <NewsList newsList={breakingNews} />
-    </View>
+      <NewsList newsList={news} />
+    </ScrollView>
   )
 }
 
---
+++ components/Loading.tsx
@@ -0,0 +1,27 @@
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native"
+import React from "react"
+
+type Props = {}
+
+const Loading = (
+  props: Props
+  //   props: React.JSX.IntrinsicAttributes &
+  //     React.JSX.IntrinsicClassAttributes<ActivityIndicator> &
+  //     Readonly<ActivityIndicator>
+) => {
+  return (
+    <View style={styles.container}>
+      <ActivityIndicator {...props} />
+    </View>
+  )
+}
+
+export default Loading
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
